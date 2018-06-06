888c98a2ccc174d2456232677d2df93289672598
ss9501 <1162882015@qq.com>
2018-06-06 11:05:33 +0800

    fixed main area overflow

--- js/lib/isBelong.js
+++ js/lib/isBelong.js
@@ -1,5 +1,5 @@
 export const isBelong = (child, parent) => {
-    while (child.tagName && child.tagName.toUpperCase() != 'BODY') {
+    while (child && child.tagName && child.tagName.toUpperCase() != 'BODY') {
 
         if (child == parent){
             return true;
