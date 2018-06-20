4cd54159fa03885b293bff4bcfec85ab20d4cde6
ss9501 <1162882015@qq.com>
2018-06-20 10:36:22 +0800

    add pagination notify message

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ export {WebHandler} from './js/web/WebHandler';
 export {deepMerge} from './js/fun/deepMerge';
 export {handler} from './js/handler';
 export {urlParser} from './js/web/lib/urlParser';
+
+export {Pagination} from './js/web/view/Pagination';
+export {notify} from './js/web/view/notify';
+export {message} from './js/web/view/message';
